7415534b976be04b1edfbf5d93d08d1d14a207d5
eXsik <qba.skupinski@gmail.com>
2024-01-19 14:22:48 +0100

    Added login and authentication funcionality

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginViewVue from '@/views/LoginView.vue'
+import LandingViewVue from '@/views/LandingView.vue'
+import axios from 'axios'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +10,43 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginViewVue
-    }
+    },
+    {
+      path: '/landing',
+      name: 'landing',
+      component: LandingViewVue
+    },
   ]
 })
+
+router.beforeEach((to, from) => {
+  if (to.name === 'login') {
+    return true;
+  }
+
+  if (!localStorage.getItem('token')) {
+    return {
+      name: 'login'
+    }
+  }
+
+  checkTokenAuthenticity();
+})
+
+const checkTokenAuthenticity = () => {
+  axios.get('http://localhost/api/user', {
+    headers: {
+      Authorization: 'Bearer ' + localStorage.getItem('token'),
+    }
+  })
+  .then(() => {})
+  .catch((error) => {
+    localStorage.removeItem('token');
+
+    router.push({
+      name: 'login',
+    }); 
+  })  
+};
 
 export default router
